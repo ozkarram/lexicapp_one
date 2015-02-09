846ac02a12c5f8fcee58ce3a5e219bb7fd1b6e2c
Oscar Alvarez <oscaralvarez@MacBook-Pro-de-Oscar.local>
2015-02-08 23:19:12 -0600

    Initial Commit

---
+++ LexicApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LexicApp
+//
+//  Created by Oscar Alvarez on 08/02/15.
+//  Copyright (c) 2015 Oscar Alvarez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LexicApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LexicApp
+//
+//  Created by Oscar Alvarez on 08/02/15.
+//  Copyright (c) 2015 Oscar Alvarez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
